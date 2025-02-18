b58f995bb657320219f300069dc3c1d12d138758
nguyenquynhthanh2k3 <namdinhtuan5@gmail.com>
2025-02-19 00:10:40 +0700

    update

--- src/app/admin/_components/sidebar.tsx
+++ src/app/admin/_components/sidebar.tsx
@@ -46,6 +46,7 @@ const menuItems: MenuItem[] = [
             { title: 'Section 1', url: '/admin/gioi-thieu/section-1' },
             { title: 'Section 2', url: '/admin/gioi-thieu/section-2' },
             { title: 'Section 3', url: '/admin/gioi-thieu/section-3' },
+            { title: 'Section 4', url: '/admin/gioi-thieu/section-4' },
         ],
     },
     {
